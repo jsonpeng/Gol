3864e26b45f9fa0a69cfdfe55b065eae5fad1919
jsonpeng <1127915086@qq.com>
2018-10-31 22:09:19 +0800

    集成gol详情

--- app/Http/Controllers/Front/PayController.php
+++ app/Http/Controllers/Front/PayController.php
@@ -66,8 +66,8 @@ class PayController extends Controller
     public function return(Request $request)
     {
         $config = $this->alipay_config;
-        $config['notify_url'] = 'http://'.$_SERVER["HTTP_HOST"].$config['notify_url'];
-        $config['return_url'] = 'http://'.$_SERVER["HTTP_HOST"].$config['return_url'];
+        $config['notify_url'] = 'https://'.$_SERVER["HTTP_HOST"].$config['notify_url'];
+        $config['return_url'] = 'https://'.$_SERVER["HTTP_HOST"].$config['return_url'];
         $data = Pay::alipay($config)->verify(); // 是的，验签就这么简单！
         $input = $request->all();
         $log_id = explode('_', $input['out_trade_no'])[1];
@@ -89,8 +89,8 @@ class PayController extends Controller
     public function notify()
     {
         $config = $this->alipay_config;
-        $config['notify_url'] = 'http://'.$_SERVER["HTTP_HOST"].$config['notify_url'];
-        $config['return_url'] = 'http://'.$_SERVER["HTTP_HOST"].$config['return_url'];
+        $config['notify_url'] = 'https://'.$_SERVER["HTTP_HOST"].$config['notify_url'];
+        $config['return_url'] = 'https://'.$_SERVER["HTTP_HOST"].$config['return_url'];
         $alipay = Pay::alipay($config);
     
         try{
