dcf581201d5af15ce3c6c2e62559754d6e8ca43d
jsonpeng <1127915086@qq.com>
2018-11-05 11:42:59 +0800

    ss

--- resources/views/front/post/index.blade.php
+++ resources/views/front/post/index.blade.php
@@ -33,8 +33,8 @@
 				<div class="detail-title text-center reveal1">
 					<h4>{!! $post->name !!}</h4>
 					<p class="mt15 mb25">
-						<span>发布者 : Gol</span>
-						<span class="news-date ml25">{!! time_parse($post->created_at)->format('Y/m/d') !!}</span>
+				{{-- 		<span>发布者 : Gol</span> --}}
+						<span class="news-date ">{!! time_parse($post->created_at)->format('Y/m/d') !!}</span>
 					</p>
 				</div>
 				<div class="detail-content">
