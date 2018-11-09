c64538b3fada59d7a6bf3dd91beca9dce2c18cb0
jsonpeng <1127915086@qq.com>
2018-11-09 17:05:33 +0800

    完善gol详情

--- resources/views/gols/table.blade.php
+++ resources/views/gols/table.blade.php
@@ -35,7 +35,7 @@
                         <td>
                             {!! Form::open(['route' => ['gols.destroy', $gol->id], 'method' => 'delete']) !!}
                             <div class='btn-group'>
-                        <!--         <a href="{!! route('gols.show', [$gol->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a> -->
+                                <a href="/golDetail/{!! $gol->id !!}" target="_blank" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
                                 <a href="{!! route('gols.edit', [$gol->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
                                 {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('确定删除吗?')"]) !!}
                             </div>
